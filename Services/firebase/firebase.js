75ca9012471a6ba107c9ec5a55b32445be27d0e4
Joao <joaoviniciusvitral@hotmail.com>
2022-11-01 14:15:02 -0300

    feat: adding firebase config

---
+++ Services/firebase/firebase.js
@@ -0,0 +1,23 @@
+
+//import firebase from 'firebase';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import 'firebase/compat/storage';
+import { getFirestore } from "@firebase/firestore"
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+})
+
+export const auth = app.auth()
+export default app
+
+export const db = getFirestore(app)
+
+export var storage = firebase.storage();
